16b4dd97725d93e818b42c8db0d184f4e504b2a6
PavanPuppala16 <pavankumarp@fixitytech.com>
2022-12-15 11:25:41 +0530

    me done

---
+++ GitWork/gitMain.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitWork
+{
+    internal class gitMain
+    {
+       public static void  me()
+        {
+            Console.WriteLine("hi");
+        }
+    }
+}
